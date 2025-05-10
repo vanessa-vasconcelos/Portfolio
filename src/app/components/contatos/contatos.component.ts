1de610caf4bb1a470060a93e29e4dedec48ade42
Vanessa Vasconcelos <vanessa.vasconcelos434@gmail.com>
2025-05-10 15:55:07 -0300

    Navegação centralizado

---
+++ src/app/components/contatos/contatos.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-contatos',
+  imports: [],
+  templateUrl: './contatos.component.html',
+  styleUrl: './contatos.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class ContatosComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { NavegacaoComponent } from '../../components/navegacao/navegacao.component';
 import { ConhecimentoComponent } from '../../components/conhecimento/conhecimento.component';
 import { ProjetosComponent } from '../../components/projetos/projetos.component';
+import { ContatosComponent } from '../../components/contatos/contatos.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavegacaoComponent, ConhecimentoComponent, ProjetosComponent],
+  imports: [NavegacaoComponent, ConhecimentoComponent, ProjetosComponent, ContatosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   encapsulation: ViewEncapsulation.None,
