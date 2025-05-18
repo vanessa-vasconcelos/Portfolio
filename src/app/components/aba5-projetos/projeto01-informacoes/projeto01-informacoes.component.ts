419c45833d33eedf540c476d944a09b1a8ac8043
Vanessa Vasconcelos <vanessa.vasconcelos434@gmail.com>
2025-05-17 21:28:12 -0300

    Aba Projetos: Criação de pasta sobre detalhamento de cada projeto

---
+++ src/app/components/aba5-projetos/projeto01-informacoes/projeto01-informacoes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projeto01-informacoes',
+  imports: [],
+  templateUrl: './projeto01-informacoes.component.html',
+  styleUrl: './projeto01-informacoes.component.scss'
+})
+export class Projeto01InformacoesComponent {
+
+  
+}
--- src/app/components/aba5-projetos/projetos.component.ts
+++ src/app/components/aba5-projetos/projetos.component.ts
@@ -1,10 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Projeto01InformacoesComponent } from './projeto01-informacoes/projeto01-informacoes.component';
 
 @Component({
   selector: 'app-projetos',
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule, Projeto01InformacoesComponent],
   templateUrl: './projetos.component.html',
   styleUrl: './projetos.component.scss',
 })
-export class ProjetosComponent {}
+export class ProjetosComponent {
+  mostrarDetalhes: boolean = false;
+
+  exibir() {
+    this.mostrarDetalhes = true;
+  }
+}
