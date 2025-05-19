a93e0ec06febac2886ca854aa33d0961aad16628
Vanessa Vasconcelos <vanessa.vasconcelos434@gmail.com>
2025-05-19 19:53:07 -0300

    Aba Conhecimento: Implementação das informações sobre cada linguagem

--- src/app/components/aba4-conhecimento/conhecimento.component.ts
+++ src/app/components/aba4-conhecimento/conhecimento.component.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-conhecimento',
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './conhecimento.component.html',
   styleUrl: './conhecimento.component.scss',
 })
-export class ConhecimentoComponent {}
+export class ConhecimentoComponent {
+  descricaoLinguagem: string = ''
+
+  mostrarDescricao(texto: string){
+    this.descricaoLinguagem = texto
+  }
+
+  sumirDescricao(){
+    this.descricaoLinguagem = ''
+  }
+}
