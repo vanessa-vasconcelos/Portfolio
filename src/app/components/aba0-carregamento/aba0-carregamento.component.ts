280fa236fa6e0fa8636e9c89edb16714b8cb0aa3
Vanessa Vasconcelos <vanessa.vasconcelos434@gmail.com>
2025-05-20 13:51:26 -0300

    Aba Projetos: Impelementação do texto explicação de cada projeto.

---
+++ src/app/components/aba0-carregamento/aba0-carregamento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aba0-carregamento',
+  imports: [],
+  templateUrl: './aba0-carregamento.component.html',
+  styleUrl: './aba0-carregamento.component.scss'
+})
+export class Aba0CarregamentoComponent {
+
+}
