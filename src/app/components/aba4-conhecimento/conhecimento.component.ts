cdd3269b3962ba2fd1707a2f045f1b71a263c4ec
Vanessa Vasconcelos <vanessa.vasconcelos434@gmail.com>
2025-05-22 18:07:44 -0300

    Aba Conhecimento: Alteração na organização das linguagens e implementação do click

--- src/app/components/aba4-conhecimento/conhecimento.component.ts
+++ src/app/components/aba4-conhecimento/conhecimento.component.ts
@@ -9,13 +9,60 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './conhecimento.component.scss',
 })
 export class ConhecimentoComponent {
-  descricaoLinguagem: string = ''
+  descricaoLinguagem: string | null = null;
 
-  mostrarDescricao(texto: string){
-    this.descricaoLinguagem = texto
-  }
+  conhecimentos = [
+    {
+      icone: 'html5-icon',
+      descricao: 'HTML é a linguagem de marcação padrão para criar e estruturar o conteúdo de páginas web. Serve para definir a estrutura e o significado do conteúdo, como parágrafos, títulos, listas, imagens e links.'
+    },
+    {
+      icone: 'css3-icon',
+      descricao: 'CSS é uma linguagem de estilização utilizada para controlar a apresentação visual de páginas web, como a cor do texto, fontes, espaçamento e layout.'
+    },
+    {
+      icone: 'sass-icon',
+      descricao: 'SASS é uma linguagem de extensão para CSS que oferece recursos adicionais para facilitar a escrita e organização de estilos em páginas web.'
+    },
+    {
+      icone: 'javascript-icon',
+      descricao: 'JavaScript é uma linguagem de programação amplamente utilizada para adicionar interatividade a páginas da web.'
+    },
+    {
+      icone: 'typescript-icon',
+      descricao: 'TypeScript é um superconjunto de JavaScript que adiciona digitação estática opcional e recursos avançados ao JavaScript.'
+    },
+    {
+      icone: 'angular-icon',
+      descricao: 'Angular é um framework de desenvolvimento front-end open-source baseado em TypeScript, uma linguagem de programação que estende o JavaScript.'
+    },
+    {
+      icone: 'java-icon',
+      descricao: 'Java é uma linguagem de programação orientada a objetos, multiplataforma, muito utilizada para criar diversos tipos de aplicações...'
+    },
+    {
+      icone: 'spring-icon',
+      descricao: 'O Spring torna a programação em Java mais rápida, fácil e segura para todos. É o framework Java mais popular do mundo.'
+    },
+    {
+      icone: 'python-icon',
+      descricao: 'Python é uma linguagem de programação de alto nível, interpretada, orientada a objetos e de tipagem dinâmica.'
+    },
+    {
+      icone: 'numpy-icon',
+      descricao: 'NumPy é uma biblioteca Python que realiza o processamento de matrizes n-dimensionais e computação numérica.'
+    },
+    {
+      icone: 'pandas-icon',
+      descricao: 'Pandas é uma biblioteca Python usada para manipulação e análise de dados.'
+    },
+    {
+      icone: 'git-icon',
+      descricao: 'Git é um sistema de controle de versão distribuído utilizado para rastrear e gerenciar as alterações em projetos.'
+    }
+  ];
 
-  sumirDescricao(){
-    this.descricaoLinguagem = ''
+  mostrarDescricao(texto: string) {
+    this.descricaoLinguagem = this.descricaoLinguagem === texto ? null : texto;
   }
 }
