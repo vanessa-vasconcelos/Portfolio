b0272b95998fa9e23992039e4b3e2401a4262e50
Vanessa Vasconcelos <vanessa.vasconcelos434@gmail.com>
2025-05-25 01:42:58 -0300

    responsividade inicio refatorada

--- src/app/components/aba3-sobre-mim/sobre-mim.component.ts
+++ src/app/components/aba3-sobre-mim/sobre-mim.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-sobre-mim',
   imports: [],
   templateUrl: './sobre-mim.component.html',
-  styleUrl: './sobre-mim.component.scss'
+  styleUrl: './sobre-mim.component.scss',
 })
 export class SobreMimComponent {
-
+  linkCurriculo =
+    'https://drive.google.com/file/d/1r8cwQZs0bSjf_3zcO-DElwGeaqFZP15g/view?usp=sharing';
 }
